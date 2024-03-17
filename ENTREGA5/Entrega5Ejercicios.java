be76bb8d95d41e247dd4bcd73dea03f6ad26dd9a
alexarnaiz04 <115986338+alexarnaiz04@users.noreply.github.com>
2024-03-17 20:25:30 +0100

    Add files via upload

---
+++ ENTREGA5/Entrega5Ejercicios.java
@@ -0,0 +1,35 @@
+//Ejercicio 18
+/*
+ * Maven es como tu equipo de construcción para proyectos Java. Organiza todo el proceso de construcción, desde obtener los materiales 
+ * hasta ensamblarlo todo, para poder enfocarte en construir tu proyecto sin tener que preocuparte por los detalles de la gestión del 
+ * proyecto.
+ * 
+ * El repositorio central de Maven es una fuente confiable de bibliotecas de software de código abierto para proyectos Java, se utiliza 
+ * para almacenar y compartir dependencias de proyectos.
+ * 
+ * El repositorio local de Maven es como tu propia colección privada de bibliotecas de software que has descargado para usar en los 
+ * proyectos.
+ */
+
+
+//Ejercicio 20
+/*
+ * Un "arquetipo" en Maven es una plantilla que te ayuda a crear proyectos de software con una estructura básica y configuraciones predeterminaadas. Es como un modelo que 
+ * puedes usar para empezar tu proyecto sin tener que empezar desde cero. Esto te ayuda a ahorrar tiempo y asegurarte de seguir las mejores prácticas de desarrollo.
+ * 
+ * El archivo pom.xml contiene toda la información necesaria para gestionar y construir el proyecto con Maven, incluyendo sus dependencias, configuraciones de compilación y plugins
+ * utilizados.
+ * 
+ * Validate: Maven verifica que tu proyecto esté configurado correctamente antes de hacer algo más. Se asegura de que los archivos y la información necesaria estén en su lugar.
+ * Compile: Maven convierte tu código Java en un formato que la computadora pueda entender y ejecutar. Es como convertir tus palabras escritas en un lenguaje que alguien más pueda leer.
+ * Test: Maven ejecuta pruebas en tu código para asegurarse de que funcione correctamente. Es como revisar un automóvil para ver si todas las piezas están en su lugar y funcionan correctamente.
+ * Package: Maven agrupa todo lo necesario para que tu programa funcione en un solo archivo, como un frasco (JAR) o un archivo web (WAR). Es como poner todas tus cosas en una maleta antes de un viaje.
+ * Install: Maven instala el archivo empaquetado en tu propia computadora para que otros proyectos puedan usarlo. Es como guardar un libro en tu estantería para que puedas leerlo cuando quieras.
+ * Deploy: Maven envía tu programa a un lugar en línea donde otras personas puedan encontrarlo y usarlo. Es como publicar un libro para que cualquiera pueda comprarlo en una librería.
+ * Clean: Maven limpia tu proyecto, eliminando cualquier archivo que haya sido creado durante el proceso de construcción. Es como limpiar tu habitación después de terminar un proyecto.
+ * Site: Maven crea un sitio web que explica tu proyecto y cómo usarlo. Es como hacer un folleto o una guía para que otros sepan de qué se trata tu proyecto y cómo pueden beneficiarse de él.
+ */
+
+ 
+ 
+ //El resto de ejercicios no estan anotados pero se ha podido realizar todo correctamente.No se si debemos mandarlo ya que solo es seguir paso a paso cada instruccion. He visto que todo me funcionbó correctamente.
